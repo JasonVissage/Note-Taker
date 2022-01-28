94f72d7b89c7312ff444042bfa018dbdd0a9da73
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-28 06:55:59 -0600

    add apiRoutes and htmlRoutes. Add router.get and router.post

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/notes', (req, res) => {
+    let savedNotes = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+    results = JSON.parse(savedNotes);
+    console.log(results)
+  res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    // let postedNotes = fs.writeFile("../db/db.json")
+    const pushedNotes = req.body;
+    res.json(pushedNotes);
+})
+
+
+module.exports = router;
