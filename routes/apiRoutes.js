cf897716dde49b322fbb6b95b421bff270c6b2a6
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-30 14:30:54 -0600

    add :id get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,17 +5,16 @@ const savedNotes = JSON.parse(fs.readFileSync('./db/db.json'));
 
 router.get('/notes/', (req, res) => {
   res.json(savedNotes);
+  // res.json(savedNotes[req.params.id])
 });
 
-// router.get('/notes/:id', (req,res) => {
-//   res.json(notes[req.params.id]);
-// })
+router.get('/notes/:id', (req,res) => {
+  res.json(notes[req.params.id]);
+})
 
 router.post('/notes', (req, res) => {
   // this way saves the notes but can't edit or delete
     let pushedNotes = req.body;
-  // this way does not save notes but I think I need the ID to edit notes
-    // let pushedNotes = req.body.id;
     savedNotes.push(pushedNotes);
     console.log('Saved!')
     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
