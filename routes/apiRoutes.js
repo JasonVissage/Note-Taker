b77a28a50aeae8edc0099d7638b17ff7af632120
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-30 13:45:22 -0600

    can save notes now. Can't edit or delete.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,19 @@ const fs = require('fs');
 const savedNotes = JSON.parse(fs.readFileSync('./db/db.json'));
 
 
-router.get('/notes', (req, res) => {
+router.get('/notes/', (req, res) => {
   res.json(savedNotes);
 });
 
+// router.get('/notes/:id', (req,res) => {
+//   res.json(notes[req.params.id]);
+// })
+
 router.post('/notes', (req, res) => {
+  // this way saves the notes but can't edit or delete
     let pushedNotes = req.body;
+  // this way does not save notes but I think I need the ID to edit notes
+    // let pushedNotes = req.body.id;
     savedNotes.push(pushedNotes);
     console.log('Saved!')
     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
